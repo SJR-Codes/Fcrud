64dfa1d14a05320fc118ce0fc2b46108d515a4bc
Samu Reinikainen <samu.reinikainen@gmail.com>
2023-01-18 10:12:28 +0200

    testing list handling in content

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    pageTitle = "Index page"
+    indexContent = "Welcome to index page"
+    testCont = ['1. First','2. Second','3. Third']
+    return render_template("index.html", title=pageTitle, content=indexContent, listcontent=testCont)
 
 # run blocks of code only if our program is the main program executed
 if __name__ == "__main__":
