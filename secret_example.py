4e7af1a7ef06757981583c59e2bcc8f407a37a7e
Samu Reinikainen <samu.reinikainen@gmail.com>
2023-01-19 12:38:31 +0200

    added class FlashSecret

--- secret_example.py
+++ secret_example.py
@@ -8,8 +8,12 @@
 #add secret.py to .gitignore
 #rename this file as secret.py
 #insert your MongoDB username & password & url to your database
+#insert string flashSecret
 
 class MongoUser():
     username = "" #MongoDB username
     passwd = "" #MongoDB password
     murl = "" #MongoDB url
+
+class FlashSecret():
+    flashSecret = b"" #add secret string for flash
