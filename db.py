9e19e1757bf9d2b0c060999cac75cfd4c4f6f543
Samu Reinikainen <samu.reinikainen@gmail.com>
2023-01-19 13:36:52 +0200

    refactored connectToMongo

--- db.py
+++ db.py
@@ -13,46 +13,47 @@ def connectToMongo():
     try:
         c = f"mongodb+srv://{MU.username}:{MU.passwd}@{MU.murl}/?retryWrites=true&w=majority"
         conn = MongoClient(c)
-        return conn['TestiDB']
+        db = conn['TestiDB']
+        return db['Testi']
     except ConnetionError:
         print("Can't connect to MongoDB")
 
 def get_all_blogs(db):
-    blogs_collection = db['Testi']
+    #blogs_collection = db['Testi']
     #tarkistus onko tyhjä collection
-    blogs = len(list(blogs_collection.find()))
+    blogs = len(list(db.find()))
     if blogs == 0:
         #HUOM: palautetaan listana
         return [{"title": "Nada Blogs found!"}]
     else:
-        return blogs_collection.find() #hakee kaikki
+        return db.find() #hakee kaikki
 
 def get_blog(db, id):
-    blogs_collection = db['Testi']
+    #blogs_collection = db['Testi']
     myquery = { "_id": ObjectId(id) }
     
-    return blogs_collection.find_one(myquery) #hakee yhden
+    return db.find_one(myquery) #hakee yhden
 
 def delete_blog(db, id):
-    blogs_collection = db['Testi']
+    #blogs_collection = db['Testi']
     myquery = { "_id": ObjectId(id) }
     
-    return blogs_collection.delete_one(myquery) #poistaa yhden
+    return db.delete_one(myquery) #poistaa yhden
 
 def create_blog(db, form):
-    blogs_collection = db['Testi']
+    #blogs_collection = db['Testi']
     title = form['title']
     snippet = form['snippet']
     body = form['body']
 
     new_blog = { "title": title, "snippet": snippet, "body": body }
 
-    x = blogs_collection.insert_one(new_blog)
+    x = db.insert_one(new_blog)
 
     return(x.inserted_id)
 
 def update_blog(db, id, form):
-    blogs_collection = db['Testi']
+    #blogs_collection = db['Testi']
     title = form['title']
     snippet = form['snippet']
     body = form['body']
@@ -60,7 +61,7 @@ def update_blog(db, id, form):
     myquery = { "_id": ObjectId(id) }
     newvalues = { "$set": { "title": title, "snippet": snippet, "body": body } }
 
-    x = blogs_collection.update_one(myquery, newvalues)
+    x = db.update_one(myquery, newvalues)
 
     return(x)
 
